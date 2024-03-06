a293779f284e6d6e779bef4683fe56306160c1da
richman0116 <maria8partee@gmail.com>
2024-03-06 02:52:53 -0800

    fix Spelling mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="   mt-52">
-      <h1 className=" text-3xl  text-center"> wellcome to manningcompany store</h1>
+      <h1 className=" text-3xl  text-center"> Welcome to Manning Company Store</h1>
     </div>
   );
 }
