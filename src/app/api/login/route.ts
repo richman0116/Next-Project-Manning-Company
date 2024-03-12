ae97aa9310cf11d2baa1f7b97be069077570a272
richman0116 <maria8partee@gmail.com>
2024-03-11 18:38:46 -0700

    fix login api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest){
         const {email, password} = reqBody;
         console.log(reqBody);
 
-        //check if user exists
+   
         const user = await User.findOne({email})
         if(!user){
             return NextResponse.json({error: "User does not exist"}, {status: 400})
@@ -22,20 +22,20 @@ export async function POST(request: NextRequest){
         console.log("user exists");
         
         
-        //check if password is correct
+      
         const validPassword = await bcryptjs.compare(password, user.password)
         if(!validPassword){
             return NextResponse.json({error: "Invalid password"}, {status: 400})
         }
         console.log('user', user);
         
-        //create token data
+  
         const tokenData = {
             id: user._id,
             username: user.username,
             email: user.email
         }
-        //create token
+      
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1d"})
 
         const response = NextResponse.json({
